88d7f5422cd6921d00706d955ead348ecde2539d
surrogatesoul <surrogatesoul@proton.me>
2024-11-13 19:20:50 +0300

    update in_nix_shell function

--- zsh-nix-shell.zsh
+++ zsh-nix-shell.zsh
@@ -1,9 +1,9 @@
 function in_nix_shell() {
   if [ ! -z ${IN_NIX_SHELL+x} ];
-    then echo "❄️  nix-shell ";
+    then echo "🐚 nix-shell";
   fi
 }
 
-RPS1="%F{yellow}%b$(in_nix_shell)%f$RPS1"
+RPS1="%F{yellow}%b$(in_nix_shell)%f"
 
 export SIMPLE_ZSH_NIX_SHELL_BASH=${0:A:h}/bash.sh
